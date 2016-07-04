1bb59b190ae574137f0e9cab4dd31dd9bdea6afb
stu <jmam0513@gmail.com>
2016-07-04 12:03:43 +0900

    Signed-off-by: stu <jmam0513@gmail.com>

---
+++ IgibuU/src/main/java/com/IgibuU/model/dto/Member.java
@@ -0,0 +1,56 @@
+package com.IgibuU.model.dto;
+
+import java.sql.Date;
+
+
+public class Member {
+
+	private String memberId;
+	private String passwd;
+	private String name;
+	private Date regDate;
+	private String gender;
+	private String memberType;
+	
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMemberType() {
+		return memberType;
+	}
+	public void setMemberType(String memberType) {
+		this.memberType = memberType;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+	
+	
+
+}
